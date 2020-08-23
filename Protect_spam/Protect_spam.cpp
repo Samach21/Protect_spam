0ae452b9c6ddf271c61f2a5aeecea98321394d70
Samach21 <69465282+Samach21@users.noreply.github.com>
2020-08-23 16:04:34 +0700

    random

--- Protect_spam/Protect_spam.cpp
+++ Protect_spam/Protect_spam.cpp
@@ -1,7 +1,22 @@
 #include<stdio.h>
+#include <stdlib.h> 
+#include <time.h> 
+
+void printEx(int, int, int);
 
 int main()
 {
-
+    srand(time(0));
+    printEx(0, 10, 4);
 	return 0;
+}
+
+void printEx(int lower, int upper, int count)
+{
+    int i;
+    for (i = 0; i < count; i++) {
+        int num = (rand() %
+            (upper - lower + 1)) + lower;
+        printf("%d ", num);
+    }
 }
